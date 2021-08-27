3f0d7911a5955c413808f2b1bd29ebf29072ded3
Ari Rudiana <arirudiana.com@Ari-Rudiana.local>
2021-08-27 09:42:20 +0700

    Campaign repo

---
+++ handler/campaign.go
@@ -0,0 +1,7 @@
+package handler
+
+// tangkap parameter di handler
+// handler ke service
+// service yg menentukan repository
+// repository : FindAll, FindByUserID
+// db
--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"fundraising/auth"
+	"fundraising/campaign"
 	"fundraising/handler"
 	"fundraising/helper"
 	"fundraising/user"
@@ -25,6 +27,22 @@ func main() {
 	}
 
 	userRepository := user.NewRepository(db)
+	campaignRepository := campaign.NewRepository(db)
+
+	campaigns, err := campaignRepository.FindByUserID(1)
+
+	fmt.Println("debug")
+	fmt.Println("debug")
+	fmt.Println("debug")
+	fmt.Println(len(campaigns))
+	for _, campaign := range campaigns {
+		fmt.Println(campaign.Name)
+
+		if len(campaign.CampaignImages) > 0 {
+			fmt.Println(campaign.CampaignImages[0].FileName)
+		}
+	}
+
 	userService := user.NewService(userRepository)
 	authService := auth.NewService()
 
