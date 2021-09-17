c0badc6bd4415dbddeaa2cfed170cb7a8f24a6f2
Ari Rudiana <arirudiana.com@Ari-Rudiana.local>
2021-09-17 19:03:51 +0700

    user transaction API

--- handler/transaction.go
+++ handler/transaction.go
@@ -40,3 +40,19 @@ func (h *transactionHandler) GetCampaignTransactions(c *gin.Context) {
 	response := helper.APIResponse("campaign's transaction", http.StatusOK, "success", transaction.FormatCampaignTransactions(transactions))
 	c.JSON(http.StatusOK, response)
 }
+
+func (h *transactionHandler) GetUserTransactions(c *gin.Context) {
+	currentUser := c.MustGet("currentUser").(user.User)
+	userID := currentUser.ID
+
+	transactions, err := h.service.GetTransactionsByUserID(userID)
+	if err != nil {
+		response := helper.APIResponse("Failed to get user transaction", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.APIResponse("user's transaction", http.StatusOK, "success", transaction.FormatUserTransactions(transactions))
+	c.JSON(http.StatusOK, response)
+
+}
--- transaction/repository.go
+++ transaction/repository.go
@@ -8,6 +8,7 @@ type repository struct {
 
 type Repository interface {
 	GetByCampaignID(campaignID int) ([]Transaction, error)
+	GetByUserID(userID int) ([]Transaction, error)
 }
 
 func NewRepository(db *gorm.DB) *repository {
@@ -24,4 +25,15 @@ func (r *repository) GetByCampaignID(campaignID int) ([]Transaction, error) {
 	}
 
 	return transaction, nil
+}
+
+func (r *repository) GetByUserID(userID int) ([]Transaction, error) {
+	var transactions []Transaction
+
+	err := r.db.Preload("Campaign.CampaignImages", "campaign_images.is_primary = 1").Where("user_id = ?", userID).Order("id desc").Find(&transactions).Error
+	if err != nil {
+		return transactions, err
+	}
+
+	return transactions, nil
 }
